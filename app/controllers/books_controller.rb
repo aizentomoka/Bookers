c8e4d6c3aa482a74210cf32ed1c501797a317591
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-11-14 08:53:29 +0000

    Showページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
   def new
-    
+    @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+  
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
@@ -17,6 +29,15 @@ class BooksController < ApplicationController
   
   def destroy
    
+  end
+  
+
+
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
