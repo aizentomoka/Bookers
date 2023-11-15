28823c584d322c77f28340eb360e397cfa160e06
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-11-15 02:33:27 +0000

    [Finish]destroy機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,12 +28,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to '/books'
    
   end 
   
   def destroy
-   
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
 
